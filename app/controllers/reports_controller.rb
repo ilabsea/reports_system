04b18028ea2a441e1bdf447a87232309ef8285c3
Sokmesa KHIEV <mesa@instedd.org>
2018-09-04 09:56:10 +0700

    Revert "add filter"
    This reverts commit d6f45f9500f4639944182c97c6a0c638bc925a9c.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,3 @@
-require 'will_paginate/array'
 class ReportsController < ApplicationController
 
   before_action :authenticated_api_user
@@ -8,9 +7,8 @@ class ReportsController < ApplicationController
     [:from, :to, :phone_number, :location, :limit, :offset].each do |key|
       param[key] = params[key] if params[key]
     end
-    # @reports = Service::request_report(session["email"], session["auth_token"], param)
-    request = Service::request_report(session["email"], session["auth_token"], param)
-    @reports = request.paginate(page: params[:page], per_page: 20)
+    @reports = Service::request_report(session["email"], session["auth_token"], param)
+    @total = @reports.size
   end
   
 end
